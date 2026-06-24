8934e08d6f3af19d31912793d5e5283a41eb98ac
SabnamJuhi <sjuhisarkars95@gmail.com>
2026-06-24 16:06:20 +0530

    nhsh

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require("../models/user");
 const { generateToken } = require("../utils/jwt");
 
 exports.register = async (req, res) => {
